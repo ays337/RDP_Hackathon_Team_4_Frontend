ca7ca923d67bac22c55d03d41c367b55a8b8f93f
Jaden Soroka <jaden.c.soroka@sherwin.com>
2025-08-08 10:06:00 -0400

    backend get requests set up

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,13 @@ from pydantic import BaseModel
 
 class db_request(BaseModel):
     table: str
-    # new_sku: tuple(int, int, str, )
 
 app = FastAPI()
 
 @app.get("/access")
 def access_db(request: db_request):
     conn = sqlite3.connect("warehouse_data.db")
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
     cursor.execute(f"SELECT * FROM {request.table}")
     rows = cursor.fetchall()
@@ -21,11 +21,12 @@ def access_db(request: db_request):
 @app.get("/access/{sku_id}")
 def access_sku(request: db_request, sku_id: int):
     conn = sqlite3.connect("warehouse_data.db")
+    conn.row_factory = sqlite3.Row
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {request.table} WHERE id = ?", (sku_id,))
-    row = cursor.fetchone()
+    cursor.execute(f"SELECT * FROM {request.table} WHERE sku_id=?", (sku_id,))
+    row = cursor.fetchall()
     conn.close()
-    return {"sku": row}
+    return {f"{request.table}": row}
 
 # @app.post("/insert-sku")
 # def insert_data(request: db_request):
